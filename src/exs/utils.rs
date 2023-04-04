472232bbe4b8f1776c310ad7e8b4aa200fddcbab
Shiyaam Sunder <shiyaamsunder@users.noreply.github.com>
2023-04-04 22:43:37 +0530

    added docstrings

--- src/exs/utils.rs
+++ src/exs/utils.rs
@@ -3,11 +3,20 @@ use std::{
     str::FromStr,
 };
 
+/// Prints the msg without a new line and flushes out the stdin
+/// # Arguments
+/// * `msg` - A string slice that is displayed as the message.
 pub fn printwoln(msg: &str) {
     print!("{}", msg);
     io::stdout().flush().unwrap();
 }
 
+/// Generic input function to get user input from the command line.
+/// # Example
+///
+/// ```
+/// let x: u32 = exs::input().expect("Entered value is not an integer");
+/// ```
 pub fn input<T: FromStr>() -> Result<T, T::Err> {
     let mut x = String::new();
     io::stdin()
